a0fad53baec35e035f552d6c71c44e3e69b746c2
mudassar211 <muhammad.mudassar1221@gmail.com>
2022-11-16 08:34:42 +0500

    Update app.js

--- Color Changing App/app.js
+++ Color Changing App/app.js
@@ -2,7 +2,6 @@
 let colors = [ 'red', 'blue', 'green', 'orange', 'purple', 'black', 'yellow'];
 
 
-
 //change the background of canvas when button is clicked
 let button = document.getElementById('button');
 
